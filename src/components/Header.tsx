385fd27234bd6e3711132c980600fd8d7cae90f9
mterrera29 <matiasterrera29@gmail.com>
2025-04-07 20:31:34 -0300

    router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export default function Header() {
+  const { pathname } = useLocation();
+
+  const isHome = pathname === '/';
+
+  console.log(isHome);
   return (
     <header className=' bg-slate-800'>
       <div className=' mx-auto container px-5 py-16'>
@@ -31,6 +36,44 @@ export default function Header() {
             </NavLink>
           </nav>
         </div>
+        {isHome && (
+          <form className='md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6'>
+            <div className='space-y-4'>
+              <label
+                htmlFor='ingredient'
+                className=' block text-white uppercase font-extrabold text-lg'
+              >
+                Nombre o Ingredientes
+              </label>
+              <input
+                id='ingredient'
+                type='text'
+                className='p-3 w-full rounded-lg focus:outline-none bg-amber-50'
+                placeholder='Nombre o Ingrediente. Ej. Vodka, Tequila, Café'
+              />
+            </div>
+            <div className='space-y-4'>
+              <label
+                htmlFor='ingredient'
+                className=' block text-white uppercase font-extrabold text-lg'
+              >
+                Categoría
+              </label>
+              <select
+                id='ingredient'
+                name='ingredient'
+                className='p-3 w-full rounded-lg focus:outline-none bg-amber-50'
+              >
+                <option value=''>-- Seleccione --</option>
+              </select>
+            </div>
+            <input
+              type='submit'
+              value='Buscar Recetas'
+              className='cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase'
+            />
+          </form>
+        )}
       </div>
     </header>
   );
