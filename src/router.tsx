227f3efd6e024e7d87d38932464487633fd29ec6
mterrera29 <matiasterrera29@gmail.com>
2025-04-07 19:28:22 -0300

    router

---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import IndexPage from './views/IndexPage';
+import FavoritesRoute from './views/FavoritesRoute';
+import Layouts from './layouts/Layouts';
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layouts />}>
+          <Route path='/' element={<IndexPage />} />
+          <Route path='/favorites' element={<FavoritesRoute />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ src/views/FavoritesRoute.tsx
@@ -0,0 +1,3 @@
+export default function FavoritesRoute() {
+  return <div>FavoritesRoute</div>;
+}
