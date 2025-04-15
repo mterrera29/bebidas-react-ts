c275927cea71d7584490bf7b4a9b15c2c47e5425
mterrera29 <matiasterrera29@gmail.com>
2025-04-15 14:05:55 -0300

    modularice

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 import {
   CategoriesAPIResponseSchema,
+  DrinkAPIResponse,
   DrinksArrayAPIResponse,
   SearchRecipeSchema,
 } from '../utils/recipes-schema';
@@ -11,3 +12,5 @@ export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
 export type SearchFilter = z.infer<typeof SearchRecipeSchema>;
 
 export type Drinks = z.infer<typeof DrinksArrayAPIResponse>;
+
+export type Drink = z.infer<typeof DrinkAPIResponse>;
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,7 +1,24 @@
+import { useMemo } from 'react';
+import { useAppStore } from '../stores/useAppStore';
+import DrinkCard from '../components/DrinkCard';
+
 export default function IndexPage() {
+  const drinks = useAppStore((state) => state.drinks);
+  const hasDrinks = useMemo(() => drinks.drinks.length, [drinks]);
   return (
     <>
-      <div>Index</div>
+      <h1 className=' text-6xl font-extrabold'>Recetas</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 my-10 gap-10'>
+        {hasDrinks ? (
+          drinks.drinks.map((drink) => (
+            <DrinkCard key={drink.idDrink} drink={drink} />
+          ))
+        ) : (
+          <p className='my-10 text-center text-2xl'>
+            No hay resultados aún, utilize el formulario
+          </p>
+        )}
+      </div>
     </>
   );
 }
