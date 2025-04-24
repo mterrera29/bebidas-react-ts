82377e9f41ac7b7f8fa5ac55e5b40e4e9286ddee
mterrera29 <matiasterrera29@gmail.com>
2025-04-24 20:15:22 -0300

    favorites

--- src/views/FavoritesRoute.tsx
+++ src/views/FavoritesRoute.tsx
@@ -1,3 +1,22 @@
+import { useMemo } from 'react';
+import DrinkCard from '../components/DrinkCard';
+import { useAppStore } from '../stores/useAppStore';
+
 export default function FavoritesRoute() {
-  return <div>FavoritesRoute</div>;
+  const favorites = useAppStore((state) => state.favorites);
+  const hasFavorites = useMemo(() => favorites.length, [favorites]);
+  return (
+    <>
+      <h1 className=' text-6xl font-extrabold'>Favoritos</h1>
+      {hasFavorites ? (
+        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 my-10 gap-10'>
+          {favorites.map((recipe) => (
+            <DrinkCard key={recipe.idDrink} drink={recipe} />
+          ))}
+        </div>
+      ) : (
+        <p>Aquí se mostraran los favoritos</p>
+      )}
+    </>
+  );
 }
