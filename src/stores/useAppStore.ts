9a45075b94605dfb0da1edcfbed64e4f561c33ca
mterrera29 <matiasterrera29@gmail.com>
2025-04-24 00:07:35 -0300

    favorites

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,11 @@
 import { create } from 'zustand';
 import { createRecipeSlice, RecipeSliceType } from './recipeSlice';
 import { devtools } from 'zustand/middleware';
+import { FavoritesSliceType, createFavoritesSlice } from './favoriteSlice';
 
-export const useAppStore = create<RecipeSliceType>()(
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType>()(
   devtools((...a) => ({
     ...createRecipeSlice(...a),
+    ...createFavoritesSlice(...a),
   }))
 );
